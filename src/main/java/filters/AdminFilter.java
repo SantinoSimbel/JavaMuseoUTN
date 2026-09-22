e19b654a4c28fca74d3cc2c0a837adaee9307da9
Santino S <141889640+SantinoSimbel@users.noreply.github.com>
2026-09-22 14:45:28 -0300

    Creacion adminFilter y funcionamiento correcto

---
+++ src/main/java/filters/AdminFilter.java
@@ -0,0 +1,55 @@
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import entities.UserSessionDTO;
+
+/**
+ * Servlet Filter implementation class AdminFilter
+ */
+@WebFilter(urlPatterns = {"/CategoryServlet", "/ItemServlet"})
+public class AdminFilter extends HttpFilter implements Filter {
+       
+    public AdminFilter() {
+        super();
+    }
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession(false);
+
+        UserSessionDTO userDTO = null;
+
+        if (session != null) {
+            userDTO = (UserSessionDTO) session.getAttribute("user");
+        }
+
+        if (userDTO != null && "admin".equals(userDTO.getRole())) {
+            chain.doFilter(request, response);
+        } else {
+            resp.sendRedirect("index.jsp");
+        }
+
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
